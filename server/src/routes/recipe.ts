bc0e61bb5917c456040453632e37649848fabf9d
Brady H <oskiekid@gmail.com>
2024-11-20 01:01:51 -0600

    Added code comments documenting endpoints

--- server/src/routes/recipe.ts
+++ server/src/routes/recipe.ts
@@ -2,16 +2,41 @@ import express, { NextFunction, Request, Response } from "express";
 import log4js from "log4js";
 import { prisma } from "../db/client";
 
+// Create a new express router
 const router = express.Router();
 
 // Create a logger for the file 'recipe.ts'
 const logger = log4js.getLogger("recipe.ts");
 
-// /api/recipe/
+
+/**
+ *                                     RECIPE  /api/recipe
+ * 
+ * METHODS:
+ *    POST: Fetch all recipes
+ *        Parameters: None
+ *        Returns: Array of recipe dictionaries in the form:
+ *       
+        [
+          {
+              "name": string,
+              "instructions": string,
+              "recipe_id": string // (1.1, 1.2, 2.2 Idk really know why it is, ask kansas.)
+          },
+          {
+              etc...
+          }
+        ]
+        GET, PUT, DELETE:  Not implemented
+ * 
+ */
 router.route("/")
   .all((req: Request, res: Response, next: NextFunction) => {
+    // logging the request
     logger.info("Request to /api/recipe/");
     console.log("Request to /api/recipe/");
+
+    // Pass to Method
     next();
   })
   .post(async (req: Request, res: Response) => {
